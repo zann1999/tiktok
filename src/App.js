a74490485d6751ae007b2f4b029c6bf3467d308d
Your Name <you@example.com>
2023-01-03 12:06:16 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,14 @@
-import clsx from "clsx";
-import { Routes, Route } from "react-router-dom";
-import "./App.css";
+import clsx from 'clsx';
+import { Routes, Route } from 'react-router-dom';
 
-import { useState, useEffect } from "react";
-const api = "https://jsonplaceholder.typicode.com/posts";
+import { useState, useEffect } from 'react';
+
 function App() {
-  const [data, setdata] = useState([]);
-  useEffect(() => {
-    fetch(api)
-      .then((datajson) => datajson.json())
-      .then((datajs) => setdata(datajs));
-  }, []);
-  console.log(data);
-  return (
-    <div className="App">
-      {data.map((dt) => {
-        return (
-          <ul>
-            <li>{dt.title}</li>
-          </ul>
-        );
-      })}
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>ABC</h1>
+        </div>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
-import "./index.css";
-import App from "~/App";
-import reportWebVitals from "./reportWebVitals";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from '~/App';
+import reportWebVitals from './reportWebVitals';
+import GlobalStyles from '~/components/GlobalStyles';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>
+    <React.StrictMode>
+        <GlobalStyles>
+            <Router>
+                <App />
+            </Router>
+        </GlobalStyles>
+    </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
